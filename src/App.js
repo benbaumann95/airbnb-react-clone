40c60b5709f51a10b5275741c1f2c756a7c60442
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-05 15:02:51 +0100

    set state of flat, use ajax for json

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
-  render() {
-    const flat = {
-    "name": "Super 60m2 in trendy neighborhood!",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg",
-    "price": 150,
-    "priceCurrency": "EUR",
-    "lat": 48.885312,
-    "lng": 2.341225
-  };
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json'
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        })
+      })
+    }
+
+  render() {
     return (
       <div className='app'>
         <div className='main'>
           <div className='search'>
           </div>
           <div className='flats'>
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
           </div>
         </div>
         <div className='map'>
